c86f2fd62c72f2a8d4f44d6251f1ee65e32812a2
Bosy12 <46898914+Bosy12@users.noreply.github.com>
2022-07-29 16:28:12 +0200

    v0
    v0

---
+++ RestAssured/src/test/java/Enitities/Post.java
@@ -0,0 +1,35 @@
+package Enitities;
+
+public class Post {
+	private int Id ; 
+	private int userId ; 
+	private String body; 
+	private String title; 
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	
+	
+	
+}
